92faa17bc82a3dc0306fad7008b8219f555c862a
Tim Tilberg <ttilberg@gmail.com>
2022-02-07 11:35:34 -0600

    Rotating Boat! Collisions need work again.

--- app/main.rb
+++ app/main.rb
@@ -2,35 +2,47 @@ require 'app/nokia.rb'
 
 def tick(args)
   init(args) if args.tick_count == 0
-  player = args.state.player
-
-  map = args.state.map
 
   try_to_move(args)
   audio(args)
 
-  args.nokia.sprites << map
-  args.nokia.sprites << player
+  args.nokia.sprites << args.state.map
+  args.nokia.sprites << args.state.player
 end
 
 def audio(args)
-  unless args.audio[:fx]
-    args.audio[:bg]&.gain = 1.0
-  end
-
-  return if args.audio[:bg]
-
-  args.audio[:bg] = {
+  args.audio[:bg] ||= {
     input: 'sounds/loop-1.wav',
     looping: true
   }
+
+  # Only play a single channel. If there's fx, mute the bg.
+  if args.audio[:fx]
+    args.audio[:bg].gain = 0.0
+  else
+    args.audio[:bg].gain = 1.0
+  end
 end
+
+INPUT_ANGLE = {
+  [1,   0] => 0,
+  [1,   1] => 45,
+  [0,   1] => 90,
+  [-1,  1] => 135,
+  [-1,  0] => 180,
+  [-1, -1] => 225,
+  [0,  -1] => 270,
+  [1,  -1] => 315
+}
 
 def try_to_move(args)
   return unless args.tick_count % 2 == 0
   
   inp = args.inputs
   return unless inp.up || inp.down || inp.left || inp.right
+
+  # TODO: create 45º sprites, and create json crash maps for the various angles
+  args.state.player.angle = INPUT_ANGLE[[inp.left_right, inp.up_down]]
 
   current_x = args.state.player.x_pos
   current_y = args.state.player.y_pos
@@ -50,16 +62,18 @@ def try_to_move(args)
                                    text: "Bang!",
                                    alignment_enum: 1)
 
-    args.audio[:bg][:gain] = 0.0
     args.audio[:fx] = {
-      input: 'sounds/blip.wav'
+      input: 'sounds/blip.wav',
     }
 
     return
   end
 
+
+
   args.state.player.x_pos += inp.left_right
   args.state.player.y_pos += inp.up_down
+
 
   args.state.map.x = args.state.player.x - args.state.player.x_pos
   args.state.map.y = args.state.player.y - args.state.player.y_pos
@@ -88,10 +102,8 @@ def init(args)
   args.state.map_box = args.gtk.parse_json_file('data/map.png.json').map{|row| [ [row['x'], row['y']], true ] }.to_h
   args.state.boat_pts = args.gtk.parse_json_file('data/boat_13_7_outline.png.json').map{|row| [row['x'], row['y']] }
 
-
   args.audio[:bg] = {
     input: 'sounds/loop-1-intro.wav',
-    gain: 1.0
   }
 end
 

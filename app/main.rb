48e69effaa6d87c44d893e41cb80b2a3ac495a99
Tim Tilberg <ttilberg@gmail.com>
2022-02-08 12:37:46 -0600

    Bosses are more exciting to catch

--- app/main.rb
+++ app/main.rb
@@ -3,10 +3,17 @@ require 'app/nokia.rb'
 def tick(args)
   init(args) if args.tick_count == 0
 
-  try_to_move(args)
-  check_the_water(args)
-  go_fishing(args)
+  if args.state.current_boss
+    catching_a_boss(args)
+  else
+    try_to_move(args)
+    check_the_water(args)
+    go_fishing(args)
+    render_fishing_pole(args)
+  end
+
   render_score(args)
+  render_things_in_the_water(args)
 
   audio(args)
 
@@ -17,6 +24,65 @@ end
 
 X_SPAWN = (-50..50).to_a.freeze
 Y_SPAWN = (-30..30).to_a.freeze
+
+def render_fishing_pole(args)
+  return unless args.state.player.started_fishing_at
+  elapsed = args.state.tick_count - args.state.player.started_fishing_at
+
+  if elapsed > 50
+    args.state.player.started_fishing_at = nil
+    args.state.player.catching_a_fish = nil
+    return
+  end
+
+  # Animate the pole based on time elapsed
+  sprite = case elapsed
+  when 0..3
+    1
+  when 3..7
+    2
+  when 8..11
+    3
+  when 12..15
+    4
+  when 31..40, 44..50
+    args.state.player.catching_a_fish ? 6 : 5
+  else
+    5
+  end
+
+  args.nokia.sprites << {
+    x: args.state.player.x,
+    y: args.state.player.y,
+    w: 17,
+    h: 10,
+    path: "sprites/rod-#{sprite}.png"
+  }    
+end
+
+def catching_a_boss(args)
+  case args.state.tick_count - args.state.current_boss_at
+  when 0, 40, 100, 160, 180, 196, 209, 219, 223
+    args.audio[:fx] = {
+      input: 'sounds/chirp.wav',
+    }
+  when 240
+    args.state.something_in_the_water -= [args.state.current_boss]
+    args.state.score *= 2
+    args.state.current_boss = nil
+    args.state.current_boss_at = nil
+    args.audio.bg.paused = false
+  end
+end
+
+def render_things_in_the_water(args)
+  args.state.something_in_the_water.each do |something|
+    args.nokia.solids << something.merge(
+      x: something[:x] - args.state.player.x_pos,
+      y: something[:y] - args.state.player.y_pos,
+    )
+  end
+end
 
 
 def check_the_water(args)
@@ -46,18 +112,12 @@ def check_the_water(args)
   end
 
   args.state.something_in_the_water.reject!{|el| el[:destroy_at] < args.tick_count}
-
-  args.state.something_in_the_water.each do |something|
-    args.nokia.solids << something.merge(
-      x: something[:x] - args.state.player.x_pos,
-      y: something[:y] - args.state.player.y_pos,
-    )
-  end
 end
 
 def go_fishing(args)
   return unless args.keyboard.space
   player = args.state.player
+  player.started_fishing_at = args.state.tick_count
 
   something = args.state.something_in_the_water.find do |something|
 
@@ -69,12 +129,19 @@ def go_fishing(args)
 
   return unless something
 
+  if something.boss
+    args.state.current_boss_at = args.tick_count
+    args.state.current_boss = something
+    args.audio.bg.paused = true
+    return catching_a_boss(args)
+  end
+
   args.audio[:fx] = {
     input: 'sounds/chirp.wav',
   }
 
+  args.state.player.catching_a_fish = true
   args.state.score += 1
-
   args.state.something_in_the_water -= [something]
 end
 
@@ -83,10 +150,10 @@ def render_score(args)
 
   args.nokia.labels << args.nokia
                             .default_label
-                            .merge(x: 5,
+                            .merge(x: 2,
                                    y: 5,
                                    text: args.state.score,
-                                   alignment_enum: 1)  
+                                   alignment_enum: 0)  
 end
 
 def audio(args)
